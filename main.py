5ad19c4f16789be587f88188d9e4af7a2d31de20
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-06-27 15:25:32 -0700

    Got redis running properly

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import discord
 import redis
+import os
 
 from dotenv import load_dotenv
 load_dotenv()
 
-import os
+redisserver = redis.Redis(
+  host=os.getenv('redis_hostname'),
+  port=int(os.getenv('redis_port')), 
+  password=(os.getenv('redis_password'))
+)
 
 client = discord.Client()
 
@@ -12,16 +17,24 @@ prefixchar = "."
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}, with prefix {1}'.format(client,prefixchar))
+  print('Logged in as {0.user}, with prefix {1}'.format(client,prefixchar))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content.startswith(str(prefixchar)):
-        uinput = str(message.content).split(" ")
-        command = uinput.pop(0)
-        
+  #check for prefix helps slightly with efficiency (I think)
+  #Yes, ik I should've chosen a faster lang if I wanted efficiency.
+
+  if message.content.startswith(str(prefixchar)):
+    uinput = str(message.content).split(" ")
+
+    channel = message.channel
+    if uinput[0] == str(prefixchar+'teststore'):
+      if len(uinput) >= 3:
+        redisserver.set(uinput[1], uinput[2].encode('utf-8'))
+        await channel.send(redisserver.get(uinput[1]).decode('utf-8'))
+  
 
 client.run(os.getenv('bot_token'))
