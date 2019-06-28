63db8934f5d5adb62c185962cb33d0f5579cc1dd
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-06-27 20:51:46 -0700

    Continuing, untested cur command

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 debug = True
+owner = os.getenv('owner_id')
 
-redisserver = redis.Redis(
+redis_server = redis.Redis(
   host=os.getenv('redis_hostname'),
   port=int(os.getenv('redis_port')), 
   password=(os.getenv('redis_password'))
@@ -28,20 +29,64 @@ async def on_message(message):
 
   #check for prefix first
   if message.content.startswith(str(prefixchar)):
-    uinput = str(message.content).split(" ")
+    uinput = str(message.content).split(" ", 1)
+
+    command = uinput[0].strip(prefixchar)
+
+    #try to isolate params, may be NULL
+    try:
+      params = uinput[1]
+    except IndexError:
+      params = [""]
 
     channel = message.channel
-    author = str(message.author)
+
+    #Author details
+    author_name = str(message.author)
+
+    #using this method to ensure no discrim
+    author_uname_only = author_name.split("#", 1)[0]
+    author_discrim = str(message.author.discriminator)
+    author_id = str(message.author.id)
 
     #Test commands, test response
     if debug:
-      if uinput[0] == str(prefixchar+'teststore'):
-        if len(uinput) >= 3:
-          redisserver.set(uinput[1], uinput[2].encode('utf-8'))
-          await channel.send(embed=discord.Embed(title="", description=redisserver.get(uinput[1]).decode('utf-8')))
+      if command == 'teststore':
+        split_params = params.split(" ", 1)
+        if len(split_params) >= 2:
+          redis_server.set(split_params[0], split_params[1].encode('utf-8'))
+          await channel.send(embed=discord.Embed(title="", description=redis_server.get(split_params[0]).decode('utf-8')))
         else:
-          embed = discord.Embed(title=str(message.author)+' Invalid Command', description='Command requires 2 parameters {0} given'.format(len(uinput)-1, colour=0xff3333))
+          embed = discord.Embed(title=author_name + ' Invalid Command', description='Command requires 2 parameters {0} given'.format(len(split_params)))
           await channel.send(embed=embed)
-  
 
+    if command in ['$', 'cur', 'currency']:
+      if '#' in params:
+        split_params = params.split('#', 1)
+
+      #No parameters, use ID to search  
+      if params == "":
+        #check if ID exists
+        if redis_server.exists('id_'+author_id) == 1:
+          #search using it
+          await channel.send(discord.Embed(title=author_name, description=' has {0}'.format(redis_server.get('id'+author_id).decode('utf-8'))))
+        #ID doesn't exist
+        else:
+          #Sets the author's amount to zero
+          redis_server.set('id_'+author_id, '0'.encode('utf-8'))
+          #Creates a hash table with Username -> user_discrim -> user_ID, for username searching
+          redis_server.hset('name_'+author_uname_only, author_discrim, author_id.encode('utf-8'))
+          #ID exists now, safe to get
+          await channel.send(discord.Embed(title=author_name, description=' has {0}'.format(redis_server.get('id_'+author_id).decode('utf-8'))))
+
+      #Attempts to check using username
+      user_prob_id = redis_server.hvals(params)[0]
+      if user_prob_id != '':
+        #search if exists using user_prob_id
+        if redis_server.exists(user_prob_id) == 1:
+          #finally search
+          await channel.send(discord.Embed(title=client.user.get, description=' has {0}'.format(redis_server.get(author_id).decode('utf-8'))))
+
+      else:
+        await channel.send(discord.Embed(title = author_name))
 client.run(os.getenv('bot_token'))
